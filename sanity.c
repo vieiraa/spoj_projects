d5c2b320a5d16a16d425d6222f8d8a5258f5b9c6
Jordy Vieira <jordyvieira@hotmail.com>
2017-03-15 15:24:23 -0300

    First commit

---
+++ sanity.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned long long uint64_t;
+
+typedef struct {
+    uint64_t key;
+    uint64_t prev;
+    uint64_t next;
+} list_t;
+
+int search(list_t list[], int size, uint64_t key) {
+    int i;
+
+    for (i = 0; i < size; i++)
+        if (list[i].key == key)
+            return i;
+
+    return 0;
+}
+
+int main() {
+    list_t *list = (list_t *) malloc(2 * sizeof(list_t));
+    uint64_t key[2], prev[2], next[2], keyTmp, prevTmp, nextTmp;
+    int size = 2, flag = 1;
+    list_t aux1;
+    int i = 0;
+
+    for (int i = 0; i < 2; i++) {
+        scanf(" %llx %llx %llx", &key[i], &prev[i], &next[i]);
+
+        list[i].key = key[i];
+        list[i].prev = prev[i];
+        list[i].next = next[i];
+    }
+
+    while (scanf(" %llx %llx %llx", &keyTmp, &prevTmp, &nextTmp) == 3) {
+        size++;
+        list = (list_t *) realloc(list, size * sizeof(list_t));
+
+        list[size-1].key = keyTmp;
+        list[size-1].prev = prevTmp;
+        list[size-1].next = nextTmp;
+    }
+
+    keyTmp = key[0];
+
+    do {
+        if (!keyTmp) {
+            flag = 0;
+            break;
+        }
+
+        aux1 = list[i];
+
+        i = search(list, size, aux1.next);
+
+        if (aux1.next == list[i].key && list[i].prev == aux1.key) {
+            keyTmp = aux1.next;
+        }
+        else {
+            flag = 0;
+            break;
+        }
+    } while (keyTmp != key[1]);
+
+    if (flag)
+        printf("sana\n");
+    else
+        printf("insana\n");
+
+    return 0;
+}
