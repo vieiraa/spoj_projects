dbebddba56f6c7470d4a01041b4f65faa60741e0
Jordy Vieira <jordyvieira@hotmail.com>
2017-03-15 14:45:21 -0300

    Change verification

--- sanity.c
+++ sanity.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef unsigned long long uint64_t;
 
@@ -22,8 +23,9 @@ int search(node_t list[], int size, uint64_t key) {
 int main() {
     node_t *list = (node_t *) malloc(sizeof(node_t));
     uint64_t key, prev, next;
-    node_t ptr1, ptr2;
-    int size = 0, flag = 1;
+    node_t ptr1, ptr2, aux;
+    int size = 0;
+    bool flag;
 
     while (scanf(" %llx %llx %llx", &key, &prev, &next) == 3) {
         size++;
@@ -32,32 +34,26 @@ int main() {
         list[size - 1].key = key;
         list[size - 1].prev = prev;
         list[size - 1].next = next;
-
-        if (size == 1)
-            ptr1 = list[size - 1];
-        else if (size == 2)
-            ptr2 = list[size - 1];
     }
 
-    key = ptr1.key;
+    ptr1 = list[0];
+    ptr2 = list[1];
+
+    aux = ptr1;
 
     do {
-        int i = search(list, size, key);
+        flag = false;
 
-        node_t aux1 = list[i];
-
-        i = search(list, size, aux1.next);
-
-        node_t aux2 = list[i];
-
-        if (key == aux2.prev) {
-            key = aux1.next;
+        for (int i = 0; i < size; i++) {
+            if (aux.next == list[i].key && list[i].prev == aux.key) {
+                flag = true;
+                aux = list[i];
+            }
         }
-        else {
-            flag = 0;
+
+        if (!flag)
             break;
-        }
-    } while (key != ptr2.key);
+    } while (aux.key != ptr2.key);
 
     if (flag)
         printf("sana\n");
