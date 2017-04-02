cf0648efa76cae14edfc1d88361e31f2a352a66c
Jordy Vieira <jordyvieira@hotmail.com>
2017-04-01 21:35:29 -0300

    Rewrite this sh*t, now it is acceptable by that f-ing platform

--- ada_queue.c
+++ ada_queue.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct node {
     int data;
@@ -13,48 +14,52 @@ typedef struct node note_t;
 typedef struct {
     note_t *back;
     node_t *front;
+    bool reverse;
     int size;
 } queue_t;
 
 void create(queue_t *);
-int insert(queue_t *, int, int);
-int del(queue_t *, int);
-void print(queue_t);
-void printDel(queue_t *, int);
-void printBackwards(queue_t);
-queue_t reverse(queue_t);
+int push(queue_t *, int);
+int pushBack(queue_t *, int);
+int pop(queue_t *);
+int pop_back(queue_t *);
+int toFront(queue_t *, int, bool);
+int push_back(queue_t *, int, bool);
+int front(queue_t *, bool);
+int back(queue_t *, bool);
+void reverse(queue_t *, bool *);
 void destroy(queue_t *);
 
 int main() {
     queue_t queue;
     char op[20];
     int n, t;
+    bool rev = false;
 
     create(&queue);
 
     scanf(" %d", &t);
 
     while (t--) {
-        strcpy(op, "");
-        scanf("%s", op);
+        scanf(" %s", op);
 
         if (!strcmp(op, "back"))
-            printDel(&queue, queue.size - 1);
+            back(&queue, rev);
 
         else if (!strcmp(op, "front"))
-            printDel(&queue, 0);
+            front(&queue, rev);
 
         else if (!strcmp(op, "reverse"))
-            queue = reverse(queue);
+            reverse(&queue, &rev);
 
         else if (!strcmp(op, "push_back")) {
             scanf(" %d", &n);
-            insert(&queue, queue.size, n);
+            push_back(&queue, n, rev);
         }
 
         else if (!strcmp(op, "toFront")) {
             scanf(" %d", &n);
-            insert(&queue, 0, n);
+            toFront(&queue, n, rev);
         }
     }
 
@@ -66,119 +71,90 @@ int main() {
 void create(queue_t *queue) {
     queue->front = NULL;
     queue->back = NULL;
+    queue->reverse = false;
     queue->size = 0;
 }
 
-int insert(queue_t *queue, int pos, int data) {
+int push(queue_t *queue, int data) {
     node_t *new, *aux = queue->front;
 
     new = (node_t *) malloc(sizeof(node_t));
-
-    if (!new)
-        return 0;
-
     new->data = data;
 
-    if (!aux && !pos) {
-        new->prev = NULL;
+    if (!(queue->size)) {
         new->next = NULL;
-        queue->front = new;
-        queue->back = new;
-    }
-
-    else if (aux && !pos) {
         new->prev = NULL;
-        new->next = aux;
-        aux->prev = new;
-        queue->front = new;
-
-        aux = queue->front;
-
-        for (int i = 0; i < queue->size; i++) {
-            aux = aux->next;
-        }
-
-        queue->back = aux;
-    }
-
-    else if (pos >= queue->size) {
-        aux = queue->back;
-
-        new->prev = aux;
-        new->next = NULL;
-        aux->next = new;
         queue->back = new;
+        queue->front = new;
+        queue->size++;
+
+        return 1;
     }
 
-
-/*     else if (pos > (queue->size / 2) + 1) { */
-/*         aux = queue->back; */
-
-/*          for(int i = queue->size; i > pos - 1; i--) { */
-/*             aux = aux->prev; */
-/*         } */
-
-/*         new->prev = aux->prev; */
-/*         aux->prev->next = new; */
-/*         aux->next = new; */
-/*         new->next = aux; */
-/*     } */
-
-/*     else { */
-/*         for(int i = 0; i < pos - 1; i++) */
-/*             aux = aux->next; */
-
-/*         new->next = aux->next; */
-/*         aux->next->prev = new; */
-/*         aux->next = new; */
-/*         new->prev = aux; */
-/*     } */
-
-    queue->size++;
+    new->prev = NULL;
+    new->next = aux;
+    aux->prev = new;
+    queue->front = new;
+    (queue->size)++;
 
     return 1;
 }
 
-int del(queue_t *queue, int pos) {
-    node_t *p = queue->front, *aux;
+int pushBack(queue_t *queue, int data) {
+    node_t *new, *aux = queue->back;
 
-    if (pos > (queue->size - 1) || p == NULL)
-        return 0;
+    new = (node_t *) malloc(sizeof(node_t));
+    new->data = data;
 
-    else {
-        if (!pos) {
-            queue->front = p->next;
-        }
+    if (!(queue->size)) {
+        new->next = NULL;
+        new->prev = NULL;
+        queue->back = new;
+        queue->front = new;
+        queue->size++;
 
-        else if (queue->size == 1 && !pos) {
-            queue->front = NULL;
-            queue->back = NULL;
-        }
-
-        else if (pos == queue->size - 1){
-            p = queue->back;
-            queue->back = p->prev;
-        }
-
-        /* else if (pos >= ((queue->size) / 2) + 1){ */
-        /*     p = queue->back; */
-        /*     for (int i = queue->size; i > pos; i--) { */
-        /*         aux = p; */
-        /*         p = p->prev; */
-        /*     } */
-
-        /*     aux->prev = p->prev; */
-        /* } */
-
-        /* else { */
-        /*     for (int i = 0; i < pos; i++) { */
-        /*         aux = p; */
-        /*         p = p->next; */
-        /*     } */
-
-        /*     aux->next = p->next; */
-        /* } */
+        return 1;
     }
+
+    new->prev = aux;
+    new->next = NULL;
+    aux->next = new;
+    queue->back = new;
+    (queue->size)++;
+
+    return 1;
+}
+
+int toFront(queue_t *queue, int data, bool rev) {
+    if (rev)
+        return pushBack(queue, data);
+    else
+        return push(queue, data);
+}
+
+int push_back(queue_t *queue, int data, bool rev) {
+    if (!rev)
+        return pushBack(queue, data);
+    else
+        return push(queue, data);
+}
+
+int pop(queue_t *queue) {
+    if (!(queue->size)) {
+        printf("No job for Ada?\n");
+        return 0;
+    }
+
+    node_t *p = queue->front;
+
+    printf("%d\n", p->data);
+
+    if (queue->size == 1) {
+        queue->front = queue->back = NULL;
+    }
+
+    else
+        queue->front = p->next;
 
     queue->size--;
     free(p);
@@ -186,69 +162,52 @@ int del(queue_t *queue, int pos) {
     return 1;
 }
 
-void print(queue_t queue) {
-    node_t *p = queue.front;
-
-    if (!p) {
-        printf("empty\n");
-        return;
-    }
-
-    for (int i = 0; i < queue.size; i++) {
-        printf("%d ", p->data);
-        p = p->next;
-    }
-
-    printf("\n");
-}
-
-void printDel(queue_t *queue, int pos) {
-    node_t *p;
-
+int pop_back(queue_t *queue) {
     if (!(queue->size)) {
         printf("No job for Ada?\n");
-        return;
+        return 0;
     }
 
-    if (!pos)
-        p = queue->front;
-    else
-        p = queue->back;
+    node_t *p = queue->back;
 
     printf("%d\n", p->data);
 
-    del(queue, pos);
+    if (queue->size == 1) {
+        queue->front = NULL;
+        queue->back = NULL;
+    }
+
+    else
+        queue->back = p->prev;
+
+    queue->size--;
+    free(p);
+
+    return 1;
 }
 
-void printBackwards(queue_t queue) {
-    node_t *p = queue.back;
-
-    if (!p) {
-        printf("empty!");
-        return;
+int front(queue_t *queue, bool rev) {
+    if (rev) {
+        return pop_back(queue);
     }
 
-    for (int i = queue.size; i > 0; i--) {
-        printf("%d ", p->data);
-        p = p->prev;
+    else {
+        return pop(queue);
     }
-
-    printf("\n");
 }
 
-queue_t reverse(queue_t queue) {
-    queue_t rev;
-    node_t *aux = queue.back;
+int back(queue_t *queue, bool rev) {
+    if (!rev)
+        return pop_back(queue);
 
-    create(&rev);
+    else
+        return pop(queue);
+}
 
-    for (int i = queue.size, j = 0; i > 0; i--, j++) {
-        insert(&rev, j, aux->data);
-        aux = aux->prev;
-    }
+void reverse(queue_t *queue, bool *rev) {
+    *rev = !(*rev);
 
-    destroy(&queue);
-    return rev;
+    queue->reverse = *rev;
 }
 
 void destroy(queue_t *queue) {
